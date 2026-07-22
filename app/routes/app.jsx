d06927f6ad4f0f89d0578d7428224b57f7709dd7
Greg Russell <greg@geanosproducts.com>
2026-07-22 18:28:58 +1000

    Connect Inventory module to app navigation

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -15,10 +15,11 @@ export default function App() {
 
   return (
     <AppProvider embedded apiKey={apiKey}>
-      <s-app-nav>
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
-      </s-app-nav>
+     <s-app-nav>
+   <s-link href="/app">Dashboard</s-link>
+   <s-link href="/app/inventory">Inventory</s-link>   
+    <s-link href="/app/additional">Additional page</s-link>  
+    </s-app-nav>
       <Outlet />
     </AppProvider>
   );
